17c1ac548212820c3b3b91a4316d21c3dc3468c6
Miguel <miguel.ferreira15@zonmail.pt>
2025-11-27 07:06:04 +0000

    test: create simple test for category

---
+++ src/test/java/com/money/SaveMe/unit/Service/CategoryServiceTest.java
@@ -0,0 +1,71 @@
+package com.money.SaveMe.unit.Service;
+
+import com.money.SaveMe.Model.Category;
+import com.money.SaveMe.Model.User;
+import com.money.SaveMe.Repo.CategoryRepo;
+import com.money.SaveMe.Service.CategoryService;
+import com.money.SaveMe.Utils.AuthenticationServiceUtil;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    @Mock
+    private AuthenticationServiceUtil authenticationServiceUtil;
+    @Mock
+    private CategoryRepo categoryRepo;
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @Test
+    public void testGetAllCategories(){
+        UUID uuid = UUID.randomUUID();
+        String userId = uuid.toString();
+        String email = "user@gmail.com";
+        String name = "user";
+        String password = "password";
+        Instant now = Instant.now();
+        Instant newNow = Instant.now();
+        User u = new User(userId,email,name,password, new ArrayList<>(),now,newNow);
+
+        Long id = 1L;
+        String catName = "category";
+        String description = "New description";
+        Instant createdAt = Instant.now();
+        Instant updatedAt = Instant.now();
+        Category cat = new Category(id,u,catName,description,createdAt,updatedAt);
+
+        List<Category> catList = new ArrayList<>();
+        catList.add(cat);
+        Mockito.when(authenticationServiceUtil.getCurrentUserUuid()).thenReturn(userId);
+        Mockito.when(categoryService.getAllCategories()).thenReturn(List.of(cat));
+
+        Iterable<Category> returnedCatList = categoryService.getAllCategories();
+
+        assertNotNull(returnedCatList);
+        assertTrue(returnedCatList.iterator().hasNext());
+        Category returnedCategory = returnedCatList.iterator().next();
+        assertEquals(1L, returnedCategory.getId());
+        assertEquals(catName, returnedCategory.getName());
+        assertEquals(description, returnedCategory.getDescription());
+        assertEquals(createdAt, returnedCategory.getCreatedAt());
+        assertEquals(updatedAt, returnedCategory.getUpdatedAt());
+        assertEquals(userId, returnedCategory.getUser().getId());
+    }
+
+}
